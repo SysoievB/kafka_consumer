bc9a60baaf8d6337597134e43bf5a664bf70494a
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-02-26 07:48:51 +0100

    Works

---
+++ src/main/java/com/kafka_consumer/ProductCreatedEventHandler.java
@@ -0,0 +1,18 @@
+package com.kafka_consumer;
+
+import com.shared_core_library.ProductCreatedEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@KafkaListener(topics = "product-created-event-topic")
+public class ProductCreatedEventHandler {
+
+    @KafkaHandler
+    public void handler(ProductCreatedEvent productCreatedEvent) {
+        log.info("*************Received a new event with ID: {} *************", productCreatedEvent.getTitle());
+    }
+}
